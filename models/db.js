44ec1855dc16d11ac2ccf6694137503aee7b4b57
Mohammad Dayarneh <m.daya.nutrino@gmail.com>
2025-04-21 01:59:37 +0400

    Commit

--- models/db.js
+++ models/db.js
@@ -1,8 +1,7 @@
 // db.js
 import mongoose from "mongoose";
 
-
-const MONGO_URI = "mongodb://localhost:27017";
+const MONGO_URI= "mongodb+srv://grad_project_632:<db_password>@cluster0.wo9llcy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 const DB_NAME =  "tours_booking";
 
 mongoose.set("strictQuery", false);
