1f42131f9b7bf2ef36ed4322bc4a45e9395eb57d
Mohammad Dayarneh <m.daya.nutrino@gmail.com>
2025-04-21 02:10:15 +0400

    Update

--- models/db.js
+++ models/db.js
@@ -1,7 +1,7 @@
 // db.js
 import mongoose from "mongoose";
 
-const MONGO_URI= "mongodb+srv://grad_project_632:<db_password>@cluster0.wo9llcy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const MONGO_URI= "mongodb+srv://grad_project_632:RwaujPy1vrnGeHSQ@cluster0.wo9llcy.mongodb.net/?retryWrites=true&w=majority&tls=true&appName=Cluster0"
 const DB_NAME =  "tours_booking";
 
 mongoose.set("strictQuery", false);
@@ -13,6 +13,7 @@ export async function connectDB() {
       useUnifiedTopology: true,
       dbName: DB_NAME,
     });
+    console.log("Connected to MongoDB")
   } catch (err) {
     process.exit(1);
   }
